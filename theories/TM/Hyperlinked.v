4217202b5662b5894fe256a70465ea70886cdb80
Fabian Kunze <fkunze@fkunze.de>
2020-02-20 17:58:18 +0100

    added unfolded end-lemma for Univ-correctness

--- theories/TM/Hyperlinked.v
+++ theories/TM/Hyperlinked.v
@@ -81,6 +81,83 @@ Section UniversalMachine.
        (Encode_pair (Encode_pair (Encode_Finite _) (Encode_pair Encode_bool Encode_nat))
                     (Encode_pair (Encode_Finite _) (Encode_pair Encode_bool Encode_nat)))).
 
+  
+  Definition Univ_Rel_pretty : pRel sig^+ unit 6 :=
+    fun tin '(_, tout) =>
+      forall (M : mTM sigM 1),
+        let c := (size (graph_of_TM M) + 1) in
+        forall (tp : tape sigM) (q : states M) (s1 s2 : nat) s3 s4 s5,
+        tin[@Fin0] = mapTape (fun s => inr (Retr_f (Retract := retr_sigTape_sig) s)) tp ->
+        tin[@Fin1] ≃(retr_sigGraph_sig; s1) (graph_of_TM M) ->
+        tin[@Fin2] ≃(retr_sigCurrentState_sig; s2) (halt q, index q) ->
+        isRight_size tin[@Fin3]  s3 -> isRight_size tin[@Fin4] s4 -> isRight_size tin[@Fin5] s5 ->
+        exists k (oconf : mconfig sigM (states M) 1),
+          let size := Univ_size tp q k in
+          loopM (mk_mconfig q [|tp|]) k = Some oconf /\
+          tout[@Fin0] = mapTape (fun s => inr (Retr_f (Retract := retr_sigTape_sig) s)) (ctapes oconf)[@Fin0] /\
+          tout[@Fin1]  ≃(retr_sigGraph_sig; s1) (graph_of_TM M) /\
+          tout[@Fin2]  ≃(retr_sigCurrentState_sig;  max c s2 + Univ_nice.number_of_states M +2) (halt (cstate oconf), index (cstate oconf)) /\
+          isRight_size tout[@Fin3] (max c s3) /\
+          isRight_size tout[@Fin4] (max c s4) /\
+          isRight_size tout[@Fin5] (max c s5).
+
+
+  Lemma Univ_Realise_pretty : (Univ _ _) ⊨ Univ_Rel_pretty.
+  Proof.
+    eapply Realise_monotone.
+    { apply Univ_Realise. }
+    unfold Univ_Rel_pretty,Univ_Rel.
+    unfold containsWorkingTape,containsTrans_size,containsState_size.
+    intros tin [l tout].
+    refine (Morphisms_Prop.all_impl_morphism _ _ _);intros M;hnf.
+    refine (Morphisms_Prop.all_impl_morphism _ _ _);intros tp;hnf.
+    refine (Morphisms_Prop.all_impl_morphism _ _ _);intros q;hnf.
+    refine (Morphisms_Prop.all_impl_morphism _ _ _);intros s1;hnf.
+    refine (Morphisms_Prop.all_impl_morphism _ _ _);intros s2;hnf.
+
+    intros H s3 s4 s5.
+    specialize (H [| s3;s4;s5|]).
+    
+    intros H1 H2 H3 ? ? ?.
+    specialize (H H1 H2 H3).
+    modpon H.
+    {intros i. destruct_fin i. all:cbn. all:easy. }
+
+    destruct H as (k&[tout' s']&?&?&?&?&Hout).
+    edestruct (destruct_vector1 s') as (s''&->).
+    eexists k, (mk_mconfig tout' [|s''|]).
+
+    specialize (Univ_size_nice H) as (?&H'''1&H'''2&H''1&H''2&H''3).
+    
+    specialize (Hout Fin0) as H'0;cbn in H'0.
+    specialize (Hout Fin1) as H'1;cbn in H'1.
+    specialize (Hout Fin2) as H'2;cbn in H'2.
+
+    unfold Univ_size_bound in *.
+
+    repeat eapply conj.
+    -assumption.
+    -assumption.
+    -eapply tape_contains_size_ext with (1:= H7) (2:=eq_refl).
+     rewrite H'''1. easy. 
+    -eapply tape_contains_size_ext with (1:= H8) (2:=eq_refl).
+     specialize (H'''2 s2).
+     rewrite !UnivSpaceBounds.Encode_state_hasSize in H'''2.
+     assert (Htmp:=state_index_lt q). apply Nat.lt_le_incl in Htmp. rewrite Htmp in H'''2.
+     clear - H'''2.
+     remember (Univ_nice.number_of_states M).
+     remember (size (graph_of_TM M) + 1).
+     remember ((Univ_size tp q k)[@Fin2] s2). Lia.nia.
+    -eapply isRight_size_monotone;[exact (Hout _)| ].
+     rewrite H''1. cbn -[graph_of_TM size]. reflexivity.
+    -eapply isRight_size_monotone;[exact (Hout _)| ].
+     rewrite H''2. cbn -[graph_of_TM size]. reflexivity.
+    -eapply isRight_size_monotone;[exact (Hout _)| ].
+     rewrite H''3. cbn -[graph_of_TM size]. reflexivity.
+  Qed.
+
+  
+
   Definition Univ_T_pretty M : tRel sig^+ 6 :=
     let c := proj1_sig (Univ_nice.Univ_steps_nice M) in
     fun tin k =>
