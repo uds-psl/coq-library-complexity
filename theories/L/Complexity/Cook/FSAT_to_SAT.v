c94d4d06a1c4ea06beb3feb0a1e3add5f5aa3a0a
Lennard Gäher <l.gaeher@posteo.de>
2020-02-29 21:51:09 +0100

    reintroduce kSAT, modify tseytin to produce 3SAT instances

--- theories/L/Complexity/Cook/FSAT_to_SAT.v
+++ theories/L/Complexity/Cook/FSAT_to_SAT.v
@@ -1,10 +1,12 @@
 From PslBase Require Import Base. 
-From Undecidability.L.Complexity.Cook Require Import Prelim FSAT SAT.
+From Undecidability.L.Complexity.Cook Require Import Prelim FSAT SAT kSAT.
 (*From Undecidability.L.Complexity Require Import Tactics. *)
 From Undecidability.L.Datatypes Require Import LLists. 
 Require Import Lia. 
 
 (** * eliminate ORs *)
+(** First eliminating ORs before applying the transformation allows us to omit the proof of correctness for the 
+  OR case, which isn't really a loss as it is completely similar to the AND case. *)
 
 Inductive orFree : formula -> Prop := 
   | orFreeTrue : orFree Ftrue
@@ -41,6 +43,7 @@ Qed.
 
 (** * Tseytin transformation*) 
 
+(** Relations for keeping track of which variables a cnf uses *)
 Definition varInLiteral v (l : literal) := exists b, l = (b, v).
 Definition varInClause v c := exists l, l el c /\ varInLiteral v l. 
 Definition varInCnf v cn := exists cl, cl el cn /\ varInClause v cl. 
@@ -60,17 +63,22 @@ Proof.
   intros H H1 v H2. apply H, H1, H2. 
 Qed. 
 
+(** The following functions give the individual clauses generated for the different cases. 
+    All the clauses are in 3-CNF, which is achieved by duplicating some literals. 
+    *)
+
 (*v <-> (v1 ∧ v2) *)
 Definition tseytinAnd (v v1 v2 : var) : cnf := 
-[[(false, v); (true, v1)]; [(false, v); (true, v2)]; [(false, v1); (false, v2); (true, v)]].
+[[(false, v); (true, v1); (true, v1)]; [(false, v); (true, v2); (true, v2)]; [(false, v1); (false, v2); (true, v)]].
 (*v <-> (v1 ∨ v2)*)
 Definition tseytinOr (v v1 v2 : var) : cnf := 
-  [[(false, v); (true, v1); (true, v2)]; [(false, v1); (true, v)]; [(false, v2); (true, v)]].
+  [[(false, v); (true, v1); (true, v2)]; [(false, v1); (true, v); (true, v)]; [(false, v2); (true, v); (true, v)]].
 (* v <-> ¬ v'*)
 Definition tseytinNot (v v' : var) : cnf := 
-  [[(false, v); (false, v')]; [(true, v); (true, v')]].
+  [[(false, v); (false, v'); (false, v')]; [(true, v); (true, v'); (true, v')]].
 Definition tseytinEquiv (v v' : var) : cnf := 
-  [[(false, v); (true, v')]; [(false, v'); (true, v)]]. 
+  [[(false, v); (true, v'); (true, v')]; [(false, v'); (true, v); (true, v)]]. 
+Definition tseytinTrue v : cnf := [[(true, v); (true, v); (true, v)]]. 
 
 Lemma tseytinAnd_sat a v v1 v2 : satisfies a (tseytinAnd v v1 v2) <-> (evalVar a v = true <-> (evalVar a v1 = true /\ evalVar a v2 = true)). 
 Proof. 
@@ -101,6 +109,13 @@ Proof.
     repeat (first [rewrite H1 | rewrite H2]; cbn); tauto.
 Qed.
 
+Lemma tseytinTrue_sat a v : satisfies a (tseytinTrue v) <-> evalVar a v = true. 
+Proof. 
+  unfold tseytinTrue, satisfies. 
+  destruct (evalVar a v) eqn:H1; cbn in *; repeat (rewrite H1; cbn); tauto.
+Qed. 
+
+(** Variables used by these CNFs *)
 Ltac cnf_varsIn_force := 
   match goal with [ |- cnf_varsIn _ _ ] => 
       let v := fresh "v" in let H1 := fresh "H1" in let H2 := fresh "H2" in let H3 := fresh "H3" in  
@@ -129,9 +144,15 @@ Proof.
   unfold tseytinEquiv. cnf_varsIn_force.
 Qed.
 
+Lemma tseytinTrue_cnf_varsIn v : cnf_varsIn (fun n => n = v) (tseytinTrue v). 
+Proof. 
+  unfold tseytinTrue. cnf_varsIn_force.
+Qed. 
+
+(** The main transformation function*)
 Fixpoint tseytin' (nfVar : var) (f : formula) : (var * cnf * var) := 
   match f with 
-  | Ftrue => (nfVar, [[(true, nfVar)]], S nfVar)
+  | Ftrue => (nfVar, tseytinTrue nfVar, S nfVar)
   | Fvar v => (nfVar, tseytinEquiv v nfVar, S nfVar) (*in principle, we could directly return v; however, using a fresh variable makes the proof easier *)
   | For f1 f2 => let
       '(rv1, N1, nfVar1) := tseytin' nfVar f1 in let 
@@ -149,9 +170,6 @@ Fixpoint tseytin' (nfVar : var) (f : formula) : (var * cnf * var) :=
 Definition tseytin f : var * cnf := 
   let '(repVar, N, _) := tseytin' (formula_maxVar f) f in (repVar, N). 
 
-(*a variable v represents a formula f with respect to a CNF N iff the CNF with the variable v assumed to be true is equisatisfiable to f*)
-Definition formula_repr (f : formula) (N : cnf) (v : var) := FSAT f <-> SAT ([(true, v)] :: N). 
-
 Lemma tseytin'_nf_monotonic f nf v N nf' : tseytin' nf f = (v, N, nf') -> nf' >= nf. 
 Proof. 
   revert nf v N nf'. induction f; cbn; intros nf v N nf' H; inv H; [lia | lia | | | ].
@@ -162,8 +180,10 @@ Proof.
   apply IHf in H2. inv H1. lia. 
 Qed.
 
+(** A variable v represents a formula f with respect to a CNF N iff the CNF with the variable v assumed to be true is equisatisfiable to f*)
+Definition formula_repr (f : formula) (N : cnf) (v : var) := FSAT f <-> SAT ([(true, v)] :: N). 
 
-(*for the proof of correctness, a stronger notion of representation is needed *)
+(** Tools for composing assignments on different variable ranges *)
 Definition assgn_varsIn (p : nat -> Prop) a := forall v, v el a -> p v. 
 Definition restrict a n := filter (fun v => v <? n) a.
 Definition join (a a' : assgn) := a ++ a'.
@@ -214,7 +234,6 @@ Proof.
   apply evalVar_in_iff in H2. now apply H in H2. 
 Qed.
 
-(** Lemmas for extending an assignment by another one talking about a different range of variables. *)
 Lemma join_extension_var_sat a a' (p1 p2 : nat -> Prop) v : p1 v -> assgn_varsIn p2 a' -> (forall n, not (p1 n /\ p2 n)) -> evalVar a v = evalVar (join a' a) v.
 Proof. 
   intros H1 H2 H3. 
@@ -259,6 +278,8 @@ Ltac list_equiv := let x := fresh "x" in let Hx := fresh "Hx" in
 Definition tseytin_formula_repr (f : formula) (N : cnf) (v : var) (b nf nf' : nat):= 
   (forall a, assgn_varsIn (fun n => n < b) a -> exists a', assgn_varsIn (fun n => n >= nf /\ n < nf') a' /\ SAT.satisfies (join a' a) N /\ (FSAT.satisfies a f <-> evalVar (join a' a) v = true))
   /\ (forall a, SAT.satisfies a N -> (evalVar a v = true <-> FSAT.satisfies (restrict a b) f)). 
+
+(** Individual compatibility lemmas for the different cases *)
 
 Lemma and_compat f1 f2 b: 
   (forall nf nf' v N, nf >= b -> tseytin' nf f1 = (v, N, nf') -> cnf_varsIn (fun n => n < b \/ (n >= nf /\ n < nf')) N /\ v >= nf /\ v < nf' /\ tseytin_formula_repr f1 N v b nf nf')
@@ -446,18 +467,19 @@ Proof.
     + intros H2. apply H0'. rewrite IH32. now apply evalFormula_not_iff in H2.
 Qed.
 
+(** main composed correctness result*)
 Lemma tseytin'_repr b f nf v N nf' : 
   orFree f -> varBound_formula b f -> nf >= b -> tseytin' nf f = (v, N, nf') -> cnf_varsIn (fun n => n < b \/ (n >= nf /\ n < nf')) N /\ v >= nf /\ v < nf' /\ tseytin_formula_repr f N v b nf nf'. 
 Proof. 
   intros Hor H. revert nf nf' v N. induction f; intros. 
   - inv H. cbn in H1. inv H1. split. 
-    1 : { intros v'. intros (c & [<- | []] & (l & [<- | []] & H)). destruct H as (? & H). inv H. lia. } 
+    1 : { eapply cnf_varsIn_monotonic, tseytinTrue_cnf_varsIn. cbn; intros; lia. }
     split; [ lia | split; [lia | split]]. 
     + intros a H. exists [v]; cbn. split; [ | split]. 
       * intros v' [-> | []]. lia.
-      * unfold satisfies. cbn. now rewrite Nat.eqb_refl. 
+      * apply tseytinTrue_sat. cbn. now rewrite Nat.eqb_refl. 
       * rewrite Nat.eqb_refl. cbn. unfold FSAT.satisfies. easy.
-    + intros a H. unfold satisfies in H. cbn in H. apply eqb_prop in H. unfold FSAT.satisfies; easy.
+    + intros a H%tseytinTrue_sat. rewrite H. unfold FSAT.satisfies; easy.
   - inv H. cbn in H1. inv H1. split. 
     1: { eapply cnf_varsIn_monotonic, tseytinEquiv_cnf_varsIn. cbn; intros n0; lia. }
     split; [ lia | split; [lia | split ]].
@@ -488,6 +510,7 @@ Proof.
   intros v. unfold restrict. rewrite in_filter_iff. intros (_ & H%Nat.ltb_lt). apply H.
 Qed.
 
+(** We get the correctness of Tseytin as a corollary by first showing that tseytin_formula_repr is in fact stronger than formula_repr. *)
 Proposition tseytin_formula_repr_s f N v nf nf': tseytin_formula_repr f N v (formula_maxVar f) nf nf' -> formula_repr f N v. 
 Proof. 
   intros [H1 H2]. split.
@@ -516,17 +539,94 @@ Proof.
   - lia.
 Qed.
 
-Definition reduction f := let (v, N) := tseytin (eliminateOR f) in [(true, v)] :: N. 
+(** Main reduction function. We first eliminate ORs. Again, we duplicate literals in order to obtain a 3CNF. *)
+Definition reduction f := let (v, N) := tseytin (eliminateOR f) in [(true, v); (true, v); (true, v)] :: N. 
+
+Fact clause_sat_rep3 a l: evalClause a [l] = true <-> evalClause a [l; l; l] = true. 
+Proof. 
+  rewrite !evalClause_literal_iff. firstorder. 
+Qed. 
+
+Fact cnf_sat_rep3 l c : SAT ([l] :: c) <-> SAT ([l;l;l] :: c). 
+Proof. 
+  split; intros [a H]; exists a; unfold satisfies in *; rewrite app_singleton; 
+  rewrite app_singleton in H; rewrite evalCnf_app_iff; apply evalCnf_app_iff in H as (H1 & H2); 
+  (split; [ | easy]); apply evalCnf_clause_iff; intros cl [<- | []]; rewrite evalCnf_clause_iff in H1;
+  now apply clause_sat_rep3, H1. 
+Qed. 
 
 Lemma FSAT_to_SAT f : FSAT f <-> SAT (reduction f). 
 Proof. 
   unfold reduction. destruct (tseytin (eliminateOR f)) eqn:H1. 
   apply tseytin_repr in H1 as (H1 & H2). 
-  - rewrite eliminateOR_FSAT_equiv. tauto.
+  - rewrite eliminateOR_FSAT_equiv, <- cnf_sat_rep3. tauto. 
   - apply orFree_eliminate. 
 Qed.  
 
-(** * size analysis *)
+(** The produced CNF is even a 3-CNF, so we directly get a reduction to 3-SAT, too. *)
+Hint Constructors kCNF. 
+Fact tseytinTrue_3CNF v : kCNF 3 (tseytinTrue v).
+Proof. 
+  unfold tseytinTrue. eauto. 
+Qed. 
+
+Fact tseytinAnd_3CNF v v1 v2 : kCNF 3 (tseytinAnd v v1 v2).
+Proof. 
+  unfold tseytinAnd. eauto. 
+Qed. 
+
+Fact tseytinOr_3CNF v v1 v2 : kCNF 3 (tseytinOr v v1 v2).
+Proof. 
+  unfold tseytinOr. eauto. 
+Qed. 
+
+Fact tseytinNot_3CNF v v' : kCNF 3 (tseytinNot v v').
+Proof. 
+  unfold tseytinNot. eauto. 
+Qed. 
+
+Fact tseytinEquiv_3CNF v v' : kCNF 3 (tseytinEquiv v v').
+Proof. 
+  unfold tseytinEquiv. eauto. 
+Qed. 
+
+Lemma tseytin'_3CNF nf nf' v N f: tseytin' nf f = (v, N, nf') -> kCNF 3 N. 
+Proof. 
+  revert nf nf' N v; induction f; cbn; intros nf nf' N v H. 
+  - inv H. apply tseytinTrue_3CNF. 
+  - inv H. apply tseytinEquiv_3CNF. 
+  - destruct (tseytin' nf f1) as ((rv1 & N1) & nfVar1) eqn:H1. 
+    destruct (tseytin' nfVar1 f2) as ((rv2 & N2) & nfVar2) eqn:H2. 
+    eapply IHf1 in H1. eapply IHf2 in H2. 
+    inv H. rewrite !kCNF_app. eauto using tseytinAnd_3CNF. 
+  - destruct (tseytin' nf f1) as ((rv1 & N1) & nfVar1) eqn:H1. 
+    destruct (tseytin' nfVar1 f2) as ((rv2 & N2) & nfVar2) eqn:H2. 
+    eapply IHf1 in H1. eapply IHf2 in H2. 
+    inv H. rewrite !kCNF_app. eauto using tseytinOr_3CNF.
+  - destruct (tseytin' nf f) as ((rv & N') & nf1) eqn:H1. 
+    eapply IHf in H1. inv H. 
+    apply kCNF_app. eauto using tseytinNot_3CNF. 
+Qed. 
+
+Corollary tseytin_3CNF v N f : tseytin f = (v, N) -> kCNF 3 N. 
+Proof.
+  unfold tseytin. destruct tseytin' eqn:H. destruct p as (v' & N'). apply tseytin'_3CNF in H.  
+  intros H1. inv H1. apply H. 
+Qed. 
+
+Lemma FSAT_to_3SAT f : FSAT f <-> kSAT 3 (reduction f). 
+Proof. 
+  unfold reduction. destruct (tseytin (eliminateOR f)) eqn:H1. 
+  specialize (tseytin_3CNF H1) as H3. 
+  apply tseytin_repr in H1 as (H1 & H2). 
+  - rewrite eliminateOR_FSAT_equiv. split. 
+    + intros H. unfold kSAT. rewrite <- cnf_sat_rep3. split; [lia | split; [ | tauto]].
+      rewrite app_singleton. apply kCNF_app; eauto.
+    + intros (_ & _ & H). apply cnf_sat_rep3 in H. tauto. 
+  - apply orFree_eliminate.
+Qed. 
+
+(** * size analysis : the tseytin transformation incurs a linear blowup wrt to the size measures defined for formulas and CNFs. *)
 
 Definition c__eliminateOrSize := 4.
 Lemma eliminateOR_size f : formula_size (eliminateOR f) <= c__eliminateOrSize * formula_size f. 
@@ -547,7 +647,7 @@ Proof.
   induction f; cbn; eauto.
 Qed. 
 
-Definition c__tseytinSize := 10.
+Definition c__tseytinSize := 12.
 Lemma tseytin'_size nf nf' v N f: tseytin' nf f = (v, N, nf') -> size_cnf N <= c__tseytinSize * formula_size f. 
 Proof. 
   revert nf nf' v N. induction f; cbn -[c__tseytinSize]; intros nf nf' v N H.
@@ -592,7 +692,7 @@ Qed.
 Lemma tseytin'_varBound nf nf' v N f : varBound_formula nf f -> tseytin' nf f = (v, N, nf') -> v < nf' /\ varBound_cnf nf' N. 
 Proof. 
   revert nf nf' v N. induction f; cbn; intros nf nf' v N H0 H. 
-  - inv H. split; [ lia | eauto]. 
+  - inv H. split; [ lia | ]. unfold tseytinTrue. inv H0. eauto 20.  
   - inv H. split; [ lia | ]. unfold tseytinEquiv. inv H0. eauto 20. 
   - destruct (tseytin' nf f1) as ((rv1 & N1) & nfVar1) eqn:H1. 
     destruct (tseytin' nfVar1 f2) as ((rv2 & N2) & nfVar2) eqn:H2. 
@@ -652,7 +752,7 @@ Proof.
 Qed. 
 
 Definition c__redFSize := c__eliminateOrSize * c__tseytinSize. 
-Lemma reduction_size f: size_cnf (reduction f) <= c__redFSize * formula_size f + 2. 
+Lemma reduction_size f: size_cnf (reduction f) <= c__redFSize * formula_size f + 4. 
 Proof. 
   unfold reduction. 
   destruct tseytin eqn:H1. apply tseytin_size in H1. 
@@ -666,7 +766,8 @@ Proof.
   unfold reduction. destruct tseytin eqn:H1. 
   apply tseytin_varBound in H1 as (H1 &H2). 
   constructor.
-  - repeat constructor. rewrite H1. rewrite <-eliminateOR_maxVar_eq. specialize (eliminateOR_size f). nia. 
+  - rewrite <-eliminateOR_maxVar_eq in H1. clear H2. 
+    repeat constructor; rewrite H1; specialize (eliminateOR_size f); nia. 
   - eapply varBound_cnf_monotonic, H2. 
     rewrite <- eliminateOR_maxVar_eq. rewrite eliminateOR_size. lia.
 Qed. 
@@ -709,28 +810,28 @@ Proof.
 Defined. 
 
 (** tseytinAnd *)
-Definition c__tseytinAnd := 35.
+Definition c__tseytinAnd := 43.
 Instance term_tseytinAnd : computableTime' tseytinAnd (fun v _ => (1, fun v1 _ => (1, fun v2 _ => (c__tseytinAnd, tt)))). 
 Proof. 
   extract. solverec. unfold c__tseytinAnd; solverec. 
 Defined. 
 
 (** tseytinOr *)
-Definition c__tseytinOr := 35.
+Definition c__tseytinOr := 43.
 Instance term_tseytinOr : computableTime' tseytinOr (fun v _ => (1, fun v1 _ => (1, fun v2 _ => (c__tseytinOr, tt)))). 
 Proof. 
   extract. solverec. unfold c__tseytinOr; solverec. 
 Defined. 
 
 (** tseytinNot *)
-Definition c__tseytinNot := 21.
+Definition c__tseytinNot := 29.
 Instance term_tseytinNot : computableTime' tseytinNot (fun v _ => (1, fun v' _ => (c__tseytinNot, tt))). 
 Proof. 
   extract. solverec. unfold c__tseytinNot; solverec. 
 Defined. 
 
 (** tseytinEquiv *)
-Definition c__tseytinEquiv := 21.
+Definition c__tseytinEquiv := 29.
 Instance term_tseytinEquiv : computableTime' tseytinEquiv (fun v _ => (1, fun v' _ => (c__tseytinEquiv, tt))). 
 Proof. 
   extract. solverec. unfold c__tseytinEquiv; solverec. 
