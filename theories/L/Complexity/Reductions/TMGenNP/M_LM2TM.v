89174c34ebb24cf03769f0236584f64594e8e5c2
Fabian Kunze <fkunze@fkunze.de>
2020-03-16 18:43:14 +0100

    finished realisation and started termination of BoollistToEnc

--- theories/L/Complexity/Reductions/TMGenNP/M_LM2TM.v
+++ theories/L/Complexity/Reductions/TMGenNP/M_LM2TM.v
@@ -35,7 +35,15 @@ Module BoollistToEnc.
     all:rewrite repeat_add_app,concat_app. all:easy.
   Qed.
 
+  Lemma enc_bool_perElem_size :( fun b => Code.size (enc_bool_perElem b)) <=c (fun _ => 1).
+  Proof.
+    eexists. intros []. all:cbv - [mult]. now rewrite Nat.mul_1_r. easy.
+  Qed.
+
   Arguments enc_bool_perElem : simpl never.
+  Arguments enc_bool_nil : simpl never.
+  Arguments enc_bool_closing : simpl never.
+
   
   Section M.
     Import ProgrammingTools Combinators ListTM CaseList CaseBool.
@@ -102,7 +110,7 @@ Module BoollistToEnc.
                | (b::bs),None => tout[@Fin0] ≃ bs /\ tout[@Fin1] ≃ enc_bool_perElem b++res
                | _,_ => False
                end
-               /\ isRight tin[@Fin2]
+               /\ isRight tout[@Fin2]
                /\ tout[@Fin3] = tin[@Fin3]).
 
     
@@ -117,43 +125,115 @@ Module BoollistToEnc.
       intros bs res Hbs Hres Ht2. 
       destruct H as [H|H].
       -cbn in H. TMSimp. modpon H;[]. destruct bs. now exfalso.
-       TMSimp. modpon H0;[]. modpon H1;[]. modpon H2;[]. TMSimp.
-       repeat (simple apply conj). all:try contains_ext. now isRight_mono. reflexivity. 
+       TMSimp. modpon H0;[]. modpon H1;[]. modpon H2;[]. modpon H3;[]. TMSimp.
+       repeat (simple apply conj). all:try contains_ext. 2:reflexivity. now isRight_mono. 
       -cbn in H. TMSimp. modpon H;[]. destruct bs. 2:easy. TMSimp.
-       modpon H1;[]. modpon H0; [].
+       modpon H0;[]. modpon H1; [].
        repeat (simple apply conj). all:try now isRight_mono. all:reflexivity.
-       Unshelve. 4:eauto. 2:easy.
     Qed.
+
+    Definition Ter__step time: tRel sig^+ 4 :=
+      (fun tin k =>
+           exists (bs : list bool) (res : Pro),
+             tin[@Fin0] ≃ bs 
+             /\ tin[@Fin1] ≃ res
+             /\ isRight tin[@Fin2]
+             /\ time (length bs) <= k ).
+    
+    Import PrettyBounds.BaseCode MoreList.
+    Lemma _Terminates__step :
+      { time : UpToC (fun l => 1) &
+               projT1 M__step ↓ (Ter__step time)}.
+    Proof.
+      evar (c1 : nat). evar (c0 : nat).
+      exists_UpToC (fun k => max c0 c1).
+      eapply TerminatesIn_monotone.
+      { unfold M__step. TM_Correct_noSwitchAuto. TM_Correct.
+        intros b. TM_Correct. all: eauto 1 using App'_Terminates,App'_Realise.
+       all: now (notypeclasses refine (@Reset_Terminates _ _ _ _ _);shelve).
+      }
+      intros tin k H. hnf in H. destruct H as (bs&res&Hbs&Hres&Htin2&Hl).
+      cbn -[plus]. eexists _,( match bs with [] => _ | _ => _ end).
+      split. 1:{ eexists bs;split. 2:split. all:simpl_surject. now contains_ext. now simpl_surject. reflexivity. }
+      split.
+      2:{ intros tout b (H&Hrem). TMSimp. modpon H.
+          destruct b,bs. all:try now (exfalso;assumption).
+          all:TMSimp;simpl_surject.
+          2:{ do 2 eexists. now contains_ext. unfold Reset_steps. cbv -[mult plus]. reflexivity. }
+          infTer 4. intros ? H1. modpon H1. TMSimp.
+          infTer 4. intros H2. modpon H2.  TMSimp.
+          infTer 4. intros ? ? H3. modpon H3.  TMSimp.
+          unfold App'_T. cbn.
+          infTer 6. 1,2:now simpl_surject;contains_ext.
+          { eassert (HApp:=proj2_sig (App'_steps_nice _) (enc_bool_perElem b)). hnf in HApp.
+            erewrite HApp. rewrite (correct__leUpToC enc_bool_perElem_size). reflexivity.
+          }
+          intros ? ? H4. modpon H4. TMSimp.
+          eexists. split. now contains_ext.
+          unfold Reset_steps. rewrite (correct__leUpToC enc_bool_perElem_size). reflexivity.
+      }
+      2:now smpl_upToC_solve.
+      { rewrite <- Hl. destruct bs.
+        -rewrite <- Nat.le_max_l. unfold c0. reflexivity.
+        -rewrite <- Nat.le_max_r. unfold c1.
+         change (encode_list Encode_Com (enc_bool_perElem b)) with (encode (enc_bool_perElem b)).
+         setoid_rewrite (correct__leUpToC enc_bool_perElem_size) at 1 2. 
+         unfold CaseList_steps,CaseList_steps_cons. rewrite Encode_bool_hasSize. reflexivity.
+      }
+    Qed.
+
+    Definition Terminates__step := projT1 _Terminates__step.
 
     Definition M__loop : pTM sig^+ unit 4 := While M__step.
 
-    
+    Definition Rel__loop : pRel sig^+ (unit) 4 :=
+      (fun tin '(yout,tout) =>
+           forall (bs : list bool) (res : Pro),
+             tin[@Fin0] ≃ bs 
+             -> tin[@Fin1] ≃ res
+             -> isRight tin[@Fin2]
+             -> isRight tout[@Fin0]
+               /\ tout[@Fin1] ≃ flat_map enc_bool_perElem (rev bs)++res
+               /\ isRight tin[@Fin2]
+               /\ tout[@Fin3] = tin[@Fin3]).
+
+    Lemma Realises__loop : M__loop ⊨ Rel__loop .
+    Proof.
+      eapply Realise_monotone.
+      {unfold M__loop. TM_Correct_noSwitchAuto. TM_Correct. apply Realises__step. }
+      eapply WhileInduction;intros;hnf;intros bs res Hbs Hres Ht2.
+      -hnf in HLastStep. modpon HLastStep. destruct bs. 2:easy.
+       TMSimp. easy.
+      -hnf in HStar. modpon HStar. destruct bs. easy. TMSimp. modpon HLastStep. TMSimp.
+       repeat (simple apply conj). 1,3:now isRight_mono. 2:reflexivity.
+       {setoid_rewrite flat_map_concat_map. setoid_rewrite flat_map_concat_map in HLastStep0. 
+       rewrite map_app,concat_app. cbn. autorewrite with list. cbn. contains_ext. } 
+    Qed.
 
     Definition M : pTM sig^+ unit 4 :=
-      LiftTapes (Length retr__list retr__nat) [|Fin0;Fin1;Fin2;Fin3|];;
-      LiftTapes (ChangeAlphabet (WriteValue (encode (enc_bool_closing))) _) [|Fin2|];;
-      LiftTapes (ConcatRepeat.M retr__list retr__nat) [|Fin2;Fin3;Fin1|];;
-      LiftTapes (Reset _) [|Fin2|];;
-      LiftTapes (ChangeAlphabet (WriteValue (encode  (enc_bool_nil))) _ ) [|Fin2|];;
-      LiftTapes (ChangeAlphabet (App' _) _) [|Fin2;Fin1|];;
+      LiftTapes (Length retr__list retr__nat) [|Fin0;Fin3;Fin1;Fin2|];; (*0: still bs, 3:length bs, 1,2:right *)
+      LiftTapes (ChangeAlphabet (WriteValue (encode ([] : Pro))) retr__Pro) [|Fin1|];; (* 1:res *)
+      LiftTapes (ChangeAlphabet (WriteValue (encode (enc_bool_closing))) retr__Pro) [|Fin2|];; (* 2:const for repeat *)
+      LiftTapes (ConcatRepeat.M retr__Pro retr__nat) [|Fin2;Fin3;Fin1|];; (* 2:cnst for repeat, 3:length/empty, 1:res *)
+      LiftTapes (Reset _) [|Fin2|];;(*2: empty*)
+      LiftTapes (ChangeAlphabet (WriteValue (encode  (enc_bool_nil))) retr__Pro ) [|Fin2|];;(*2:nil*)
+      LiftTapes (ChangeAlphabet (App' _) retr__Pro) [|Fin2;Fin1|];;(* 1 : res with nil part *)
+      LiftTapes (Reset _) [|Fin2|];;(*2: empty*)
       M__loop.
 
+    Lemma Realises : M ⊨ Rel .
+    Proof.
+      eapply Realise_monotone.
+      {unfold M. TM_Correct_noSwitchAuto. TM_Correct.
+       all:eauto 1 using Length_Computes, ConcatRepeat.Realises,  App'_Realise,Realises__loop, Realises__step.
+       all:now (notypeclasses refine (@Reset_Realise _ _ _ _ _);shelve). }
+      intros tin (yout,tout) H. hnf. intros bs Hbs Htin1 Htin2 Htin3.
+      hnf in H. cbn in H. TMSimp. modpon H;[]. specialize H0 with (x:=[]). modpon H0;[].
+      modpon H1;[]. modpon H2;[]. modpon H3;[].  modpon H4;[]. modpon H5;[]. modpon H6;[].
+      modpon H7;[].  repeat (simple apply conj). 1,3,4:now isRight_mono.
+      { rewrite enc_bool_explicit,rev_length. autorewrite with list in H14. contains_ext. }
+    Qed.
 
-    (*
-    Context (sig F:finType) (n:nat) (M__multi : pTM sig F n).
-
-      (*TODO: Check that tape contaisn Encoding of programm*)
-  (*
-      Definition M : pTM _ _ 12 := LiftTapes HaltingProblem.Loop [|Fin1,Fin2,|].
-
-      Definition Rel : pRel ((sigList (sigTape sig)) ^+) (option F) 1 :=
-        fun t '(y,t') => match y with
-                        None => ~ exists (v : tapes sig n), StepTM.contains_tapes t[@Fin0] v
-                      | Some y => exists (v v': tapes sig n), StepTM.contains_tapes t[@Fin0] v
-                                                        /\ StepTM.contains_tapes t'[@Fin0] v'
-                                                        /\ Canonical_Rel M__multi v (y,v')
-                      end.
-  *) *)
   End M.
 End BoollistToEnc.
 
