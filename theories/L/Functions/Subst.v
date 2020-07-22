948e0fe88c7b89458ccad51a642c43c0990e8f3a
Fabian Kunze <fkunze@fkunze.de>
2020-07-22 16:44:56 +0200

    small tweaks

--- theories/L/Functions/Subst.v
+++ theories/L/Functions/Subst.v
@@ -3,11 +3,9 @@ Require Import Nat .
 Import EqBool.
 
 (** ** Extracted substitution on terms *)
-  
 Instance term_substT :
   computableTime' subst (fun s _ => (5, (fun n _ => (1, (fun t _ => ( (size s) * (size s + c__eqbComp nat * 4 + 20), tt)))))).
 Proof.
-  unfold subst. change Nat.eqb with (EqBool.eqb (X:=nat)).
   extract. unfold eqbTime.
   recRel_prettify2. all:unfold eqbTime; cbn [fst snd size].
   all:try rewrite !size_nat_enc.
