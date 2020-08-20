835d13be24ebbe6c83c7c3352838438abf27ae08
Fabian Kunze <fkunze@fkunze.de>
2020-08-20 17:14:17 +0200

    added PCF-example for Nat.plus

--- theories/GenExtract/GenExtractPCF.v
+++ theories/GenExtract/GenExtractPCF.v
@@ -36,7 +36,7 @@ Polymorphic Inductive TT : Type -> Type :=
 Check TT.
 
 Existing Class TT.
-Existing Instance TyB.
+Existing Instance TyNat.
 Existing Instance TyArr.
   
 
@@ -52,7 +52,6 @@ Fixpoint valueContext {n} : forall (Γ : typeContext n) , Type :=
     0 => fun _ => unit
   | S n' => fun Γ => (projT1 (fst Γ) * valueContext (snd Γ))%type
   end.
-Set Printing Implicit.
 Fixpoint nthC {m} : forall {Γ : typeContext m} (n : fin m) (vs: valueContext Γ) , projT1 (nthV n Γ) :=
   match m as m' return forall  {Γ : typeContext m'} (n : fin m') (vs: @valueContext m' Γ)  , projT1 (nthV n Γ) with
   | 0 => fun _ f => False_rect _ f
@@ -63,21 +62,51 @@ Fixpoint nthC {m} : forall {Γ : typeContext m} (n : fin m) (vs: valueContext Γ
            end)
   end.
 
-
-
-Inductive isPCF : forall {n} (Γ : typeContext n) (A: anyTT), (valueContext Γ -> projT1 A) -> Type :=
+Inductive isPCF : forall n (Γ : typeContext n) (A: anyTT), (valueContext Γ -> projT1 A) -> Type :=
   isPCF_Const n Γ (c:nat) :
-    @isPCF n Γ (existT _ _ TyNat) (fun _ => c)
+    isPCF n Γ (existT _ _ TyNat) (fun _ => c)
 | isPCF_Rel n Γ x :
     let A := nthV x Γ in @isPCF n Γ A (nthC x)
 | isPCF_App n Γ t1 t2 tt1 tt2
             (f:valueContext Γ -> t1 -> t2) (arg: valueContext Γ -> t1):
-    @isPCF n Γ (existT _ _ (@TyArr t1 t2 tt1 tt2)) f
+    isPCF n Γ (existT _ _ (@TyArr t1 t2 tt1 tt2)) f
     -> @isPCF n Γ (existT _ _ tt1) arg
-    -> @isPCF n Γ (existT _ _ tt2) (fun vs => (f vs) (arg vs))
+    -> @isPCF n Γ (existT _ _ tt2) (fun ctx => (f ctx) (arg ctx))
 | isPCF_Lambda n Γ t1 t2 tt1 tt2 f:
-    @isPCF (S n) (existT _ _ tt1,Γ) (existT _ _ tt2) (fun arg' => f (snd arg') (fst arg'))
-    -> @isPCF n Γ (existT _ _ (@TyArr t1 t2 tt1 tt2)) (fun arg x => f arg x)
-(* Missing: Fixes*).
+    isPCF (S n) (existT _ _ tt1,Γ) (existT _ _ tt2) (fun ctx => f (snd ctx) (fst ctx))
+    -> @isPCF n Γ (existT _ _ (@TyArr t1 t2 tt1 tt2)) f
+             
+| isPCF_Case_nat n Γ tt x (f__O : valueContext Γ -> projT1 tt) (f__S: valueContext Γ -> nat -> projT1 tt):
+    @isPCF n Γ (existT _ _ TyNat) x
+    -> @isPCF n Γ tt f__O 
+    -> @isPCF (S n) (existT _ _ TyNat,Γ) tt (fun ctx => f__S (snd ctx) (fst ctx))
+    -> @isPCF n Γ tt (fun ctx => match x ctx return projT1 tt with 0 => f__O ctx | S n => f__S ctx n end)
+             
+| isPCF_Succ n Γ:
+    @isPCF n Γ (existT _ _ (TyArr _ _)) (fun _ => S)
+| isPCF_Fix_unaryNat n Γ t tt f F:
+    (forall (ctx : valueContext Γ) (x:nat), F ctx x = f (x,(F ctx,ctx)))
+    -> isPCF (S (S n)) (existT _ _ TyNat,(existT _ _ (@TyArr nat t _ tt),Γ)) (existT _ _ tt) f
+    -> isPCF n Γ (existT _ _ (@TyArr nat t _ tt)) (fun ctx x => F ctx x).
 
+(* Missing: Fixes, match nat*)
 
+Lemma isPCF_plus : isPCF 0 tt (existT _ _ (TyArr _ _)) (fun _ => plus).
+Proof.
+  unfold Init.Nat.add.
+  simple eapply isPCF_Fix_unaryNat with (f:= fun ctx=> fun m => match fst ctx with
+                                                          | 0 => m
+                                                          | S p => S (fst (snd ctx) p m)
+                                                          end).
+  now intros arg [].
+  simple eapply isPCF_Lambda.
+  simple eapply isPCF_Case_nat.
+  -eapply (isPCF_Rel 3) with (x:=Some None).
+  -eapply (isPCF_Rel 3) with (x:=None).
+  -eapply isPCF_App. now econstructor. cbn. 
+   eapply isPCF_App with (f:= fun ctx => fst (snd (snd (snd ctx))) (fst ctx));cbn.
+   2:now eapply (isPCF_Rel 4) with (x:=Some None).
+   eapply isPCF_App with (f:= fun ctx => fst (snd (snd (snd ctx))));cbn.
+   now eapply (isPCF_Rel 4) with (x:=Some (Some (Some None))).
+   now eapply (isPCF_Rel 4) with (x:=None).
+Qed.
